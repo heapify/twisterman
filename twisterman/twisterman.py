f4cda7dabc77466d3b0f468d18a5d1f947d60605
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2012-07-27 00:53:22 +0300

    Actually does something useful now

--- twisterman/twisterman.py
+++ twisterman/twisterman.py
@@ -1,4 +1,4 @@
-
+from os import environ
 from twisted.internet import protocol
 from twisted.application import service
 from twisted.python import log
@@ -14,35 +14,79 @@ class ProcessProtocol(service.Service, protocol.ProcessProtocol):
         self.reactor = reactor
         self.setName(name)
         self.commandline = commandline
+        self.outbuffer = ""
+        self.errbuffer = ""
+        self.running = False
     
     def connectionMade(self):
         self.printLine("started with pid %s" % str(self.transport.pid))
+        self.running = True
 
-    def processEnded(self, status):
+    def processExited(self, status):
+        self.running = False
+        if self.errbuffer:
+            self.printLine(self.errbuffer)
+            self.errbuffer = ""
+        if self.outbuffer:
+            self.printLine(self.outbuffer)
+            self.outbuffer = ""
         self.printLine("process terminated")
+        self.parent.stopService()
+
+    def outReceived(self, data):
+        self.outbuffer += data
+        while True:
+            line, _, remainder = self.outbuffer.partition("\n")
+            if not remainder:
+                break
+            else:
+                self.outbuffer = remainder
+                self.printLine(line)
+
+    def errReceived(self, data):
+        self.errbuffer += data
+        while True:
+            line, _, remainder = self.errbuffer.partition("\n")
+            if not remainder:
+                break
+            else:
+                self.errbuffer = remainder
+                self.printLine(line)
+                                                                                    
 
     def printLine(self, message):
+        message = message.rstrip()
         print "%s %s    | %s" % (datetime.now().strftime("%H:%M:%S"), self.name,
                                  message)
 
     def startService(self):
-        self.reactor.spawnProcess(self, "sh", args=('-c', self.commandline))
+        self.reactor.spawnProcess(self, "sh", args=('sh', '-c', self.commandline), env=environ)
 
 
     def stopService(self):
-        self.transport.signalProcess('KILL')
-
+        if self.running:
+            self.transport.signalProcess('KILL')
 
 class ProcessManager(service.MultiService, service.Service):
     def __init__(self, reactor=_reactor, procfile="Procfile"):
+        service.MultiService.__init__(self)
         self.reactor = reactor
         self.procfile = Procfile(procfile)
+        self.running = False
 
     def startService(self):
+        if self.running:
+            return
+        self.running = True
         for name, commandline in self.procfile.iteritems():
             protocol = ProcessProtocol(name, commandline)
             protocol.setServiceParent(self)
-            protocol.startService()
 
 
-
+    def stopService(self):
+        if not self.running:
+            return
+        self.running = False
+        for child in self:
+            child.stopService()
+        self.reactor.stop()
